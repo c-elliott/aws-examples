1f3974e068290ef9ce74c4183a960471db1f2ba5
Chris Elliott <14152935+c-elliott@users.noreply.github.com>
2020-10-24 20:34:48 +0100

    Create multiproc.py

---
+++ multiproc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from multiproces import Pool
+from time import sleep
+"""
+  This is an example of process based parallelism.
+  Note use of multiprocess from the Pathos library,
+  not the more common multiprocessing library.
+
+  https://pypi.org/project/pathos/
+
+  Multiprocess uses dill for serialisation and
+  supports class methods and other complexity.
+"""
+
+def worker_main():
+    sleep(5)
+
+def main():
+    workers = 5
+    while True:
+        try:
+            # Worklist contains data to be distributed
+            worklist = []
+            # Launch workers
+            process = Pool(workers)
+            # Map data to worker_main function
+            process.map(worker_main, worklist)
+            # Block until all work completed
+            process.close()
+            process.join()
+        except Exception as ex:
+            print(str(ex))
+
+if __name__ == '__main__':
+    main()
